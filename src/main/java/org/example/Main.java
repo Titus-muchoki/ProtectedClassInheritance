b9d47e91d237fda5aac1564ab11d7b3021db4c63
kajela <titoyut56@gmail.com>
2022-06-06 15:50:38 -0400

    add the initial files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,20 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+public class Main {
+    protected String name = "myCity";
+    protected String country = "australia";
+
+    public void myHood() {
+        System.out.println("one day i will be balling in australia");
     }
 }
+ class europe extends Main{
+    private String myDream = "queensLand";
+    public static void main (String [] args) {
+        europe myObj = new europe();
+        System.out.println(myObj.country);
+        System.out.println(myObj.name);
+        System.out.println(myObj.myDream);
+        myObj.myHood();
+    }
+    }
---
+++ src/main/java/org/example/continent.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class continent extends Main {
+    private String dream = "sydney";
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+
+
+}
